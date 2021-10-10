6971aa5249d05e2781791ec6e289113700fa88d9
PrateekOraon <oraonprateek251@gmail.com>
2021-10-10 18:51:41 +0530

    anything

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="h-screen flex items-center justify-center bg-purple-600 bg-opacity-25">
       <div className="w-4/5 h-4/5 shadow-xl bg-white dark:bg-red-200 rounded-lg p-40 text-4xl">
         <button class="btn-indigo">
-          Click me
+          Click me again
         </button>
         <Heroes />
       </div>
